ff04d8ecd36fedbf862a533e287535f1a6a4d5e0
SGWebFreelancer <sgwebfreelancer@gmail.com>
2022-10-26 11:10:30 +0800

    Added customize manytomany relationship

--- app/models.py
+++ app/models.py
@@ -45,6 +45,16 @@ class CustomUser(AbstractUser):
         """ Return string representation of our user """
         return self.email
 
+# class Product(models.Model):
+#     name = models.CharField(max_length=200)
+#     sku = models.CharField(max_length=20)
+#     price = models.DecimalField(default=0, decimal_places=2, max_digits=8)
+
+# class Order(models.Model):
+#     order_date = models.DateField()
+#     status = models.CharField(max_length=20)
+#     products = models.ManyToManyField(Product, related_name="products")
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -55,4 +65,10 @@ class Product(models.Model):
 class Order(models.Model):
     order_date = models.DateField()
     status = models.CharField(max_length=20)
-    products = models.ManyToManyField(Product, related_name="products")
+    products = models.ManyToManyField(Product, through="Detail")
+
+
+class Detail(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import UserProfile, Account, Product, Order
+from .models import Detail, UserProfile, Account, Product, Order, Detail
+from django.db import transaction
 
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -34,6 +35,18 @@ class UserSerializer(serializers.ModelSerializer):
         UserProfile.objects.create(user=user, **profile_data)
         return user
 
+# class ProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Product
+#         fields = ('id', 'name', 'price', 'sku')
+
+# class OrderSerializer(serializers.ModelSerializer):
+#     # To return products object instead of id
+#     products = ProductSerializer(read_only=True, many=True)
+#     class Meta:
+#         model = Order
+#         fields = ('id', 'order_date', 'status', 'products')
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -41,9 +54,30 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'price', 'sku')
 
 
+class DetailSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(many=False)
+
+    class Meta:
+        model = Detail
+        fields = ('id', 'product', 'quantity')
+
+
 class OrderSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(read_only=True, many=True)
+    products = DetailSerializer(source="detail_set", read_only=True, many=True)
 
     class Meta:
         model = Order
         fields = ('id', 'order_date', 'status', 'products')
+
+    @transaction.atomic
+    def create(self, validated_data):
+        order = Order.objects.create(**validated_data)
+        if "products" in self.initial_data:
+            products = self.initial_data.get("products")
+            for product in products:
+                id = product.get("id")
+                quantity = product.get("quantity")
+                product = Product.objects.get(pk=id)
+                Detail(order=order, product=product, quantity=quantity).save()
+        order.save()
+        return order
